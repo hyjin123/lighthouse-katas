02d5932899aaa93e462495d3939631eabe72edaa
Sean Jin <seanhoyeonjin@gmail.com>
2021-08-06 14:36:12 +0000

    Kata 1 Solution

---
+++ kata1.js
@@ -0,0 +1,29 @@
+// given an array with 2 or more numbers, find the two largest numbers in that array and sum them
+const sumLargestNumbers = function(data) {
+  let newArray = [];
+  let sum = 0;
+  let max = 0;
+  let secondMax = 0;
+  let maxIndex = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] > max) {
+      max = data[i];
+      maxIndex = i;
+    }
+  }
+  newArray = data.splice(maxIndex,1);
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] > secondMax) {
+      secondMax = data[i];
+      maxIndex = i;
+    }
+  }
+  newArray.push(secondMax);
+  return sum = newArray[0] + newArray[1];
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
