898f3de593b57e8fff1528d9a30df29b702d6bcf
Sean Jin <seanhoyeonjin@gmail.com>
2021-08-06 15:28:24 +0000

    Kata 3 Solution changes to comment

--- kata3.js
+++ kata3.js
@@ -1,6 +1,6 @@
 //count the number of vowels that appear in a given string
 const numberOfVowels = function(data) {
-  let sum = 0; //store the number of vowels that appear in string
+  let sum = 0; //store the number of vowels that appear in string.
   let i = 0;
   while(i < data.length) {
     if (data[i] === "a" ||
@@ -8,7 +8,7 @@ const numberOfVowels = function(data) {
         data[i] === "i" ||
         data[i] === "o" ||
         data[i] === "u") {
-      sum ++;
+      sum ++; //add 1 to the sum count whenever a vowel appears in string.
     }
     i++;
   }
