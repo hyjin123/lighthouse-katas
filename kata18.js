827a4f59a96317f98415825e60a1c9f3f9a055b7
Sean Jin <seanhoyeonjin@gmail.com>
2021-08-19 21:03:51 +0000

    Kata 18 Solution, finally!

--- kata18.js
+++ kata18.js
@@ -1,63 +1,39 @@
 //creating our own Square Code algorithm
 const squareCode = function (message) {
-
-};
-
-console.log(squareCode("chill out"));
-console.log(squareCode("feed the dog"));
-console.log(squareCode("have a nice day"));
-console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
-
-/*
-clu hlt io
-
-chi
-llo
-ut
-
-fto ehg ee dd
-
-feed
-thed
-og
-
-hae and via ecy
-imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau
-*/
-
-/*
-  let columns = 0;
   let newMessage = "";
   let newMessage2 = [];
-  for (let i = 0; i < message.length; i++) { //remove the spaces
+  for (let i = 0; i < message.length; i++) { //remove the spaces in the string
     if (message[i] !== " ") {
       newMessage += message[i];
     }
   }
 
-  for (let i = 0; i < newMessage.length; i++) {
+  for (let i = 0; i < newMessage.length; i++) { //store each letter in an array
     newMessage2[i] = newMessage[i];
   }
 
-  columns = (Math.ceil(Math.sqrt(newMessage2.length))); //number of columns
+  let columns = (Math.ceil(Math.sqrt(newMessage2.length))); //number of columns
+
   const newMessage3 = [];
-  for (let i = 0; i < newMessage2.length; i += columns) {
+  for (let i = 0; i < newMessage2.length; i += columns) { //store the letters in chunks equal to the number of columns
     const chunk = newMessage2.slice(i, i + columns);
-    newMessage3.push(chunk);
+    newMessage3.push(chunk); //push these chunks into a new array to be looped over later
   }
 
   let finalMessage = "";
+  //loop through the new array and store in new string, adding space after every time the second for loop completes
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < newMessage3.length; j++) {
       if (newMessage3[j][i] !== undefined) {
         finalMessage += newMessage3[j][i];
       }
     }
-  }
-
-  let wordsPerLine = Math.ceil(finalMessage.length / columns);
-  for (let i = 0; i < finalMessage.length; i += wordsPerLine) {
-    finalMessage = finalMessage.substring(i, wordsPerLine) + " " + finalMessage.substring(wordsPerLine, finalMessage.length)
+    finalMessage += " ";
   }
   return finalMessage;
-  */
+};
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
