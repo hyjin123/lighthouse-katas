a8169170530e36878a4a664d6076e6a4151d0ba9
Sean Jin <seanhoyeonjin@gmail.com>
2021-08-29 15:23:35 +0000

    Kata 21

---
+++ kata21/kata21.js
@@ -0,0 +1,9 @@
+//Number Guesser - User has to guess a secret number
+let prompt = require("prompt-sync")();
+
+// code below (replace this example)
+let answer = prompt("Guess a number: ");
+console.log("You answered: " + answer);
+
+//generating random number from 1-10
+//Math.floor((Math.random() * 10) + 1);
